f2c57e0c4440ffb85dbfd90658b00804ecf4d3fa
Felipe Lopes <luizfelipe.tec.info@gmail.com>
2022-12-10 13:03:48 -0300

    Resolvendo Bug - done

--- middlewares/attachCurrentUser.js
+++ middlewares/attachCurrentUser.js
@@ -1,14 +1,14 @@
-import { UserModel } from "../models/user.model.js"
+import { UserModel } from "../model/user.model.js"
 
 async function attachCurrentUser(req, res, next) {
     try {
         const userData = req.auth;
         const user = await UserModel.findOne(
-            { _id: userData._id }, 
-            { passwordHash: 0 } 
-            );
+            { _id: userData._id },
+            { passwordHash: 0 }
+        );
 
-        if (!user) { 
+        if (!user) {
             return res.status(404).json({ msg: "Usuário não encontrado!" })
         }
 
@@ -20,4 +20,4 @@ async function attachCurrentUser(req, res, next) {
     }
 }
 
-export default attachCurrentUser;
+export { attachCurrentUser } 

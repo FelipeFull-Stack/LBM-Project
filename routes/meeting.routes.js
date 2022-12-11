f4379656a3559a0011731546b40cdd221db841ea
Felipe Lopes <luizfelipe.tec.info@gmail.com>
2022-12-11 20:19:12 -0300

    meetingRouter.post and (update: User, Customer, Process) - done

--- routes/meeting.routes.js
+++ routes/meeting.routes.js
@@ -3,15 +3,45 @@ import attachCurrentUser from "../middlewares/attachCurrentUser.js";
 import isAdmin from "../middlewares/isAdmin.js";
 import isAuth from "../middlewares/isAuth.js";
 import { MeetingModel } from "../model/meeting.model.js";
+import { UserModel } from "../model/user.model.js";
+import { ProcessModel } from "../model/process.model.js";
+import { CustomerModel } from "../model/customer.model.js";
 
 const meetingRouter = express.Router();
 
 meetingRouter.post(
-    "/",
+    "/:customerId",
     isAuth,
     attachCurrentUser,
     async (req, res) => {
-
+        try {
+            const loggedInUser = req.currentUser;
+            const newMeeting = MeetingModel.create({
+                ...req.body,
+                advogado: loggedInUser._id,
+                customer: req.params.customerId,
+                process: req.params.customerId._doc.process
+            });
+            await UserModel.findOneAndUpdate(
+                { _id: loggedInUser._id },
+                { $push: { meetings: newMeeting._doc._id } },
+                { runValidators: true }
+            );
+            await CustomerModel.findOneAndUpdate(
+                { _id: req.params.customerId },
+                { meeting: newMeeting._doc._id },
+                { runValidators: true }
+            );
+            await ProcessModel.findOneAndUpdate(
+                { _id: req.params.customerId._doc.process },
+                { meeting: newMeeting._doc._id },
+                { runValidators: true }
+            );
+            return res.status(201).json(newMeeting);
+        } catch (err) {
+            console.log(`Erro em meetingRouter.post Back-end ${err}`);
+            return res.status(500).json(err);
+        }
     }
 );
 

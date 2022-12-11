2248e47ed45985e6d084f875e92af17913fd1c24
Felipe Lopes <luizfelipe.tec.info@gmail.com>
2022-12-11 18:40:28 -0300

    customerRouter.get/all - done

--- routes/customer.routes.js
+++ routes/customer.routes.js
@@ -6,20 +6,40 @@ import { CustomerModel } from "../model/customer.model.js";
 
 const customerRouter = express.Router();
 
+//criando o cadastro de um cliente
 customerRouter.post(
     "/",
     isAuth,
     attachCurrentUser,
     async (req, res) => {
-        const loggedInUser = req.currentUser;
-        const newCustomer = CustomerModel.create({
-            ...req.body,
-            advogado: loggedInUser._id
-        });
-        return res.status(201).json(newCustomer);
+        try {
+            const loggedInUser = req.currentUser;
+            const newCustomer = CustomerModel.create({
+                ...req.body,
+                advogado: loggedInUser._id
+            });
+            return res.status(201).json(newCustomer);
+        } catch (err) {
+            console.log(`Erro em CustomerRouter.post Back-end: ${err}`);
+            return res.status(500).json(err);
+        }
     }
 );
 
-
+//procurando todos os clientes
+customerRouter.get(
+    "/",
+    isAuth,
+    attachCurrentUser,
+    async (req, res) => {
+        try {
+            const customers = await CustomerModel.find({});
+            return res.status(200).json(customers);
+        } catch (err) {
+            console.log(`Erro em CustomerRouter.get/all Back-end: ${err}`);
+            return res.status(500).json(err);
+        }
+    }
+);
 
 export { customerRouter }

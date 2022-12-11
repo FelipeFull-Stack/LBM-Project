fb990a91a71512f59b3c7fca1c7d4a8c1a0b60dc
Felipe Lopes <luizfelipe.tec.info@gmail.com>
2022-12-11 20:44:36 -0300

    meetingRouter.delete and Update(User, Customer, Process) - done

--- routes/meeting.routes.js
+++ routes/meeting.routes.js
@@ -94,6 +94,7 @@ meetingRouter.get(
     }
 );
 
+//alterando a data da reunião alvo
 meetingRouter.put(
     "/:meetingId",
     isAuth,
@@ -108,6 +109,47 @@ meetingRouter.put(
             return res.status(200).json(AlteredMeeting);
         } catch (err) {
             console.log(`Erro em meetingRouter.put Back-end: ${err}`);
+            return res.status(500).json(err);
+        }
+    }
+);
+
+//deletando a reunião
+//removendo a reunião automaticamente do processo, advogado e do cliente
+meetingRouter.delete(
+    "/:meetingId",
+    isAuth,
+    attachCurrentUser,
+    async (req, res) => {
+        try {
+            const deletedMeeting = await MeetingModel.deleteOne({ _id: req.params.meetingId });
+            await UserModel.findOneAndUpdate(
+                { meetings: req.params.meetingId },
+                {
+                    $pull: { meetings: req.params.meetingId },
+                    $push: { updateAt: new Date(Date.now()) }
+                },
+                { runValidators: true }
+            );
+            await CustomerModel.findOneAndUpdate(
+                { meeting: req.params.meetingId },
+                {
+                    meeting: null,
+                    $push: { updateAt: new Date(Date.now()) }
+                },
+                { runValidators: true }
+            );
+            await ProcessModel.findOneAndUpdate(
+                { meeting: req.params.meetingId },
+                {
+                    meeting: null,
+                    $push: { updateAt: new Date(Date.now()) }
+                },
+                { runValidators: true }
+            );
+            return res.status(200).json(deletedMeeting);
+        } catch (err) {
+            console.log(`Erro em meetingRouter.delete Back-end: ${err}`);
             return res.status(500).json(err);
         }
     }
